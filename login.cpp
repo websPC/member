8d2a06b652556da695cc3982dbfbefe3f9dfeafd
jshtp123 <jshtp123@gmail.com>
2014-08-08 17:05:18 +0900

    아이디 webs 비번 1234

--- login.cpp
+++ login.cpp
@@ -17,32 +17,47 @@ Login::~Login()
 
 void Login::on_pushButton_clicked()
 {
-    QSqlTableModel *model = new QSqlTableModel();
-    model->setTable("member");
-    model->select();
-    model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
-    model->setHeaderData(1, Qt::Horizontal, QObject::tr("SEX"));
-    model->setHeaderData(2, Qt::Horizontal, QObject::tr("HP"));
-    model->setHeaderData(3, Qt::Horizontal, QObject::tr("FEES"));
-    model->setHeaderData(4, Qt::Horizontal, QObject::tr("Level"));
-    model->setHeaderData(5, Qt::Horizontal, QObject::tr("Grd"));
-    model->setHeaderData(6, Qt::Horizontal, QObject::tr("Birth"));
-    model->setHeaderData(7, Qt::Horizontal, QObject::tr("Name"));
+    QString ID = ui->lineEdit->text();
+    QString PW = ui->lineEdit_2->text();
+
+    if(ID == "webs" && PW == "1234" && ID != NULL)
+    {
+        QSqlTableModel *model = new QSqlTableModel();
+        model->setTable("member");
+        model->select();
+        model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+        model->setHeaderData(1, Qt::Horizontal, QObject::tr("SEX"));
+        model->setHeaderData(2, Qt::Horizontal, QObject::tr("HP"));
+        model->setHeaderData(3, Qt::Horizontal, QObject::tr("FEES"));
+        model->setHeaderData(4, Qt::Horizontal, QObject::tr("Level"));
+        model->setHeaderData(5, Qt::Horizontal, QObject::tr("Grd"));
+        model->setHeaderData(6, Qt::Horizontal, QObject::tr("Birth"));
+        model->setHeaderData(7, Qt::Horizontal, QObject::tr("Name"));
 
 
-    QSqlTableModel *schedule = new QSqlTableModel();
-    schedule->setTable("schedule");
-    schedule->select();
-    schedule->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
-    schedule->setHeaderData(1, Qt::Horizontal, QObject::tr("Number"));
-    schedule->setHeaderData(2, Qt::Horizontal, QObject::tr("Date"));
-    schedule->setHeaderData(3, Qt::Horizontal, QObject::tr("Title"));
-    schedule->setHeaderData(4, Qt::Horizontal, QObject::tr("Type"));
-    schedule->setHeaderData(5, Qt::Horizontal, QObject::tr("Contents"));
+        QSqlTableModel *schedule = new QSqlTableModel();
+        schedule->setTable("schedule");
+        schedule->select();
+        schedule->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+        schedule->setHeaderData(1, Qt::Horizontal, QObject::tr("Number"));
+        schedule->setHeaderData(2, Qt::Horizontal, QObject::tr("Date"));
+        schedule->setHeaderData(3, Qt::Horizontal, QObject::tr("Title"));
+        schedule->setHeaderData(4, Qt::Horizontal, QObject::tr("Type"));
+        schedule->setHeaderData(5, Qt::Horizontal, QObject::tr("Contents"));
 
-    Form *new_page = new Form(0, model, schedule);
+        Form *new_page = new Form(0, model, schedule);
 
-    new_page->show();
-    this->close();
+        new_page->show();
+        this->close();
+    }
+    else
+    {
+        QMessageBox::warning(this,tr("Fucking"),tr("Go away anna"));
+        ui->lineEdit->setText("");
+        ui->lineEdit_2->setText("");
+     }
+
+
+
 
 }
